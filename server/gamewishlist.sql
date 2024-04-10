ea57b160a927fd833799765afd223480bfc079eb
aarons767 <aarondsouza42@gmail.com>
2024-04-09 19:31:11 -0700

    initial React, Express, and Mysql project setup

---
+++ server/gamewishlist.sql
@@ -0,0 +1,39 @@
+USE wishlists;
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    Pass VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Games (
+    GameID INT PRIMARY KEY,
+    GameTitle VARCHAR(255) NOT NULL,
+    Category VARCHAR(255),
+    DateReleased DATE,
+    Publisher VARCHAR(255)
+);
+
+CREATE TABLE Platforms (
+    PlatformID INT PRIMARY KEY,
+    GameID INT,
+    Platform VARCHAR(255) NOT NULL,
+    FOREIGN KEY (GameID) REFERENCES Games(GameID)
+);
+
+CREATE TABLE WishList (
+    WishlistID INT PRIMARY KEY,
+    UserID INT,
+    GameID INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (GameID) REFERENCES Games(GameID)
+);
+
+CREATE TABLE Reviews (
+    ReviewID INT PRIMARY KEY,
+    UserID INT,
+    GameID INT,
+    Commentt TEXT,
+    Rating INT
+);
